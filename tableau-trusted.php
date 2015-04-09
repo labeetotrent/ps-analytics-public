6aa596d99219cea0810f329739ebdeeee2ea7634
Ben Sullins <ben.sullins@gmail.com>
2015-04-09 14:37:23 -0700

    Create tableau-trusted.php

---
+++ tableau-trusted.php
@@ -0,0 +1,52 @@
+<?php
+
+// Returns a trusted URL for a view on a server for the
+// given user.  For example, if the URL of the view is:
+//    http://tabserver/views/MyWorkbook/MyView
+//
+// Then:
+//   $server = "tabserver";
+//   $view_url = "views/MyWorkbook/MyView";
+//
+function get_trusted_url($user,$server,$view_url) {
+  $params = ':embed=yes&:toolbar=yes';
+
+  $ticket = get_trusted_ticket($server, $user, $_SERVER['REMOTE_ADDR']);
+  if($ticket) { //since version 8.1 the code is alphanumeric
+    return "https://$server/trusted/$ticket/$view_url?$params";
+  }
+  else 
+    return 0;
+}
+
+Function get_trusted_ticket($wgserver, $user, $remote_addr) {
+  $params = array(
+    'username' => $user,
+    'client_ip' => $remote_addr
+  );
+
+  return do_post_request("https://$wgserver/trusted", $params);
+}
+
+function do_post_request($url, $data, $optional_headers = null)
+{
+  $params = array('http' => array(
+              'method' => 'POST',
+              'content' => http_build_query($data)
+            ));
+  if ($optional_headers !== null) {
+    $params['http']['header'] = $optional_headers;
+  }
+  $ctx = stream_context_create($params);
+  $fp = @fopen($url, 'rb', false, $ctx);
+  if (!$fp) {
+    throw new Exception("Problem with $url, $php_errormsg");
+  }
+  $response = @stream_get_contents($fp);
+  if ($response === false) {
+    throw new Exception("Problem reading data from $url, $php_errormsg");
+  }
+  return $response;
+}
+
+?>
